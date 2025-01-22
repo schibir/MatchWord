37a7db992dde35e9a4de0a57fa0f9b85a71117f0
c00895316 <chibiryaev.sergey@huawei.com>
2025-01-22 16:16:43 +0300

    Add canvas and basic configuration for it

--- sources/main.ts
+++ sources/main.ts
@@ -1,5 +1,61 @@
-let words: Array<string> = ['Hello', 'World'];
 
-for (let word of words) {
-  console.log(word);
+function getRequestAnimationFrame()
+{
+  if (window.requestAnimationFrame) {
+    return window.requestAnimationFrame;
+  }
+
+  // Manual fallbacks
+  let lastTime = 0;
+  return (callback : FrameRequestCallback) => {
+    const currTime = Date.now();
+    const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    const id = setTimeout(() => callback(currTime + timeToCall), timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+  };
 }
+
+function isMobile()
+{
+  const isAndroid = () => navigator.userAgent.match(/Android/i);
+  const isBlackBerry = () => navigator.userAgent.match(/BlackBerry/i);
+  const isiOS = () => navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  const isOpera = () => navigator.userAgent.match(/Opera Mini/i);
+  const isWindows = () => navigator.userAgent.match(/IEMobile/i);
+  return isAndroid() || isiOS() || isBlackBerry() || isOpera() || isWindows();
+}
+
+function main(_ : Event)
+{
+  const requestAnimationFrame = getRequestAnimationFrame();
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  // const context = canvas.getContext("2d");
+  const mobile = isMobile();
+
+  let isRendering = false;
+
+  const update = () => {
+    if (!isRendering) {
+      return;
+    }
+
+    requestAnimationFrame(update);
+  };
+
+  if (mobile) {
+  } else {
+    canvas.addEventListener("mouseup", (_) => {
+      isRendering = false;
+    }, false);
+    canvas.addEventListener("mouseleave", (_) => {
+      isRendering = false;
+    }, false);
+    canvas.addEventListener("mousedown", (_) => {
+      isRendering = true;
+      requestAnimationFrame(update);
+    }, false);
+  }
+}
+
+window.addEventListener("load", main);
