950fe2de5b4681f557572ee5230f98466edea6b4
c00895316 <chibiryaev.sergey@huawei.com>
2025-01-24 13:33:40 +0300

    Correct move tile and other ones

--- sources/game.ts
+++ sources/game.ts
@@ -1,5 +1,16 @@
 import { TileImage, Tile } from "./tile";
 
+
+function correctCoord(coord : number, tileSize : number)
+{
+  const pos = coord / tileSize | 0;
+  return tileSize * pos;
+}
+
+function getCenter(coord : number, tileSize : number)
+{
+  return coord + tileSize / 2 | 0;
+}
 export class Game
 {
   #context : CanvasRenderingContext2D;
@@ -8,14 +19,15 @@ export class Game
   #tileWidth : number;
   #tileHeight : number;
   #currentTile : Tile | null = null;
+  #currentTilePos = [0, 0];
 
   constructor(canvas : HTMLCanvasElement)
   {
     this.#context = canvas.getContext("2d") as CanvasRenderingContext2D;
 
     const borderSize = 5;
-    this.#tileWidth = (canvas.width - borderSize) / 5;
-    this.#tileHeight = (canvas.height - borderSize) / 5;
+    this.#tileWidth = (canvas.width - borderSize) / 5 | 0;
+    this.#tileHeight = (canvas.height - borderSize) / 5 | 0;
 
     for (let x = 0; x < 5; x++) {
       for (let y = 0; y < 5; y++) {
@@ -32,27 +44,53 @@ export class Game
     for (let tile of this.#tiles) {
       tile.render(this.#context);
     }
-    if (this.#currentTile) {
-      this.#currentTile.render(this.#context);
+    this.#currentTile?.render(this.#context);
+  }
+
+  #getTile(x : number, y : number, ignoreTile : Tile | null)
+  {
+    for (let tile of this.#tiles) {
+      if (ignoreTile === tile) {
+        continue;
+      }
+      if (x > tile.x && x < tile.x + this.#tileWidth &&
+          y > tile.y && y < tile.y + this.#tileHeight) {
+            return tile;
+          }
     }
+    return null;
   }
 
   takeTile(x : number, y : number)
   {
     console.assert(this.#currentTile === null);
-    for (let tile of this.#tiles) {
-      if (x > tile.x && x < tile.x + this.#tileWidth &&
-          y > tile.y && y < tile.y + this.#tileHeight) {
-            this.#currentTile = tile;
-            break;
-          }
-    }
+    this.#currentTile = this.#getTile(x, y, null);
     console.assert(this.#currentTile !== null, `Unable to find tile in ${x}, ${y}`);
+    this.#currentTilePos = this.#currentTile ? [this.#currentTile.x, this.#currentTile.y] : [0, 0];
+  }
+
+  #checkTile(myTile : Tile)
+  {
+    if (myTile.x < 0 || myTile.x >= this.#context.canvas.width ||
+        myTile.y < 0 || myTile.y >= this.#context.canvas.height) {
+          return false;
+        }
+    for (let tile of this.#tiles) {
+      if (myTile.x === tile.x && myTile.y === tile.y) {
+        return false;
+      }
+    }
+    return true;
   }
 
   dropTile()
   {
     console.assert(this.#currentTile !== null);
+    if (this.#currentTile) {
+      this.#currentTile.x = correctCoord(getCenter(this.#currentTile.x, this.#tileWidth), this.#tileWidth);
+      this.#currentTile.y = correctCoord(getCenter(this.#currentTile.y, this.#tileHeight), this.#tileHeight);
+      console.assert(this.#checkTile(this.#currentTile));
+    }
     this.#currentTile = null;
   }
 
@@ -64,5 +102,22 @@ export class Game
     }
     this.#currentTile.x += dx;
     this.#currentTile.y += dy;
+
+    const centerX = getCenter(this.#currentTile.x, this.#tileWidth);
+    const centerY = getCenter(this.#currentTile.y, this.#tileHeight);
+    if (centerX <= 0 || centerX >= this.#context.canvas.width ||
+        centerY <= 0 || centerY >= this.#context.canvas.height) {
+          this.moveTile(-dx, -dy);
+          return;
+        }
+    const tile = this.#getTile(centerX, centerY, this.#currentTile);
+    if (!tile) {
+      return;
+    }
+    [tile.x, this.#currentTilePos[0]] = [this.#currentTilePos[0], tile.x];
+    [tile.y, this.#currentTilePos[1]] = [this.#currentTilePos[1], tile.y];
+    console.assert(correctCoord(tile.x, this.#tileWidth) === tile.x &&
+                   correctCoord(tile.y, this.#tileHeight) === tile.y);
+    console.assert(this.#checkTile(tile));
   }
 };
